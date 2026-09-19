f52f38d1816f1d45f37de806668cdd9c85414972
Andhika Rahman <andhikarhmnp@gmail.com>
2026-09-19 23:35:43 +0700

    learn: introduction to c programming language

--- 01-basics/hello_world.c
+++ 01-basics/hello_world.c
@@ -1,5 +1,20 @@
-#include <stdio.h>
+/* 
 
-int main() {
+September 19th, 2026
+Day 1 of Learning C Programming Language.
+Chapter 1: Basics, Subchapter 1: Introduction.
+
+*/
+
+/* header, required to use input/output functions such as printf() */
+#include <stdio.h> 
+
+/* the main function, execution of the program starts here */
+int main() { 
+
+    /* printf() function print some outputs to the screen */
     printf("Hello, world\n");
+
+    /* return 0 terminates the program and indicates program is successfully executed */
+    return 0;
 }
